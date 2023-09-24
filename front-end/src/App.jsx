78f255090aa395d26aaa9b2af8da1bcbc79aeda7
gherardiD <gherardi.daniele.studente@itispaleocapa.it>
2023-09-24 10:44:06 +0200

    installation of tailwind

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
-import './App.css'
+import './index.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App"></div>
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">travel wallet</h1>
+    </div>
   )
 }
 
