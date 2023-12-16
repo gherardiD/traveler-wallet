0b372db4534d3db07b8c6639b7ad58e95fb45022
gherardiD <gherardi.daniele.studente@itispaleocapa.it>
2023-12-16 09:31:53 +0100

    add: amministrator show users method style: home.php

--- other/forms/php_auth/home.php
+++ other/forms/php_auth/home.php
@@ -1,32 +1,139 @@
 <?php
 session_start();
 
-if(!isset($_SESSION["email"])){
-  header("Location: http://127.0.0.1/5ia/forms/php_auth/login.php");
-}else if($_SESSION["email"] == "dani.ghera05@gmail.com"){
-  $email = $_SESSION["email"];
-  echo "benvenuto sei l'amministratore";
+if (!isset($_SESSION["email"])) {
+    header("Location: http://127.0.0.1/5ia/forms/php_auth/login.php");
+} else if ($_SESSION["email"] == "dani.ghera05@gmail.com") {
+    $email = $_SESSION["email"];
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
 
-  echo "<br><br><a href='logout.php'>Logout</a>";
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Welcome</title>
+        <style>
+            body {
+                font-family: 'Arial', sans-serif;
+                margin: 20px;
+                background-color: #f4f4f4;
+            }
 
-  echo "<br><br><button id='getValute'>valute</button>";
+            .container {
+                max-width: 600px;
+                margin: 0 auto;
+                padding: 20px;
+                background-color: #fff;
+                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                border-radius: 5px;
+            }
 
-  // Container for the valute elements
-  echo "<div id='valuteContainer'></div>";
-}else{
-  $email = $_SESSION["email"];
-  echo "benvenuto ti sei registrato con ".$email;
+            .welcome-message {
+                font-size: 18px;
+                margin-bottom: 20px;
+            }
 
-  echo "<br><br><a href='logout.php'>Logout</a>";
+            .button {
+                padding: 10px;
+                background-color: #4caf50;
+                color: #fff;
+                text-decoration: none;
+                display: inline-block;
+                border-radius: 5px;
+            }
+
+            .container-btn{
+              display: flex;
+              flex-direction: row;
+            }
+
+            button {
+                padding: 10px;
+                background-color: #007bff;
+                color: #fff;
+                border: none;
+                border-radius: 5px;
+                cursor: pointer;
+                margin-top: 10px;
+                margin-right: 10px;
+            }
+
+            #valuteContainer,
+            #usersContainer {
+                margin-top: 10px;
+            }
+
+            #valuteContainer div,
+            #usersContainer div {
+                margin-bottom: 5px;
+                padding: 8px;
+                background-color: #eee;
+                border-radius: 3px;
+            }
+        </style>
+    </head>
+
+    <body>
+        <div class="container">
+            <p class="welcome-message">Benvenuto, sei l'amministratore</p>
+
+            <a href='logout.php' class="button">Logout</a>
+            <div class="container-btn">
+              <button id='getValute'>Valute</button>
+              
+              <button id='getUsers'>Utenti</button>
+            </div>
+            <div id='valuteContainer'></div>
+            <div id='usersContainer'></div>
+        </div>
+
+        <script>
+            // Your existing JavaScript code here
+        </script>
+    </body>
+
+    </html>
+    <?php
+} else {
+    $email = $_SESSION["email"];
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Welcome</title>
+        <style>
+            /* Add your styling for non-admin users here */
+        </style>
+    </head>
+
+    <body>
+        <div class="container">
+            <p class="welcome-message">Benvenuto, ti sei registrato con <?= $email ?></p>
+
+            <a href='logout.php' class="button">Logout</a>
+        </div>
+    </body>
+
+    </html>
+<?php
 }
-
 ?>
 
-<script>
-    const button = document.getElementById("getValute");
-    const valuteContainer = document.getElementById("valuteContainer");
 
-    button.addEventListener("click", function () {
+<script>
+    // Get the buttons and containers
+    const getValuesBtn = document.getElementById("getValute");
+    const valuteContainer = document.getElementById("valuteContainer");
+    const getUsersBtn = document.getElementById("getUsers");
+    const usersContainer = document.getElementById("usersContainer");
+
+    // Add an event listener to the getValuesBtn
+    getValuesBtn.addEventListener("click", function () {
+      if(valuteContainer.childNodes.length === 0){
         const xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function () {
             if (xhr.readyState == 4 && xhr.status == 200) {
@@ -46,5 +153,35 @@ if(!isset($_SESSION["email"])){
 
         xhr.open("GET", "get_valute.php", true);
         xhr.send();
+      }else{
+        valuteContainer.innerHTML = '';
+      }
+    });
+
+    // Add an event listener to the getUsersBtn
+    getUsersBtn.addEventListener("click", function () {
+      if(usersContainer.childNodes.length === 0){
+        const xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                const result = JSON.parse(xhr.responseText);
+
+                // Clear previous content in the usersContainer
+                usersContainer.innerHTML = '';
+
+                // Create a div for each user and append it to the usersContainer
+                result.forEach(function (user) {
+                    const userDiv = document.createElement("div");
+                    userDiv.textContent = user;
+                    usersContainer.appendChild(userDiv);
+                });
+            }
+        };
+
+        xhr.open("GET", "get_users.php", true);
+        xhr.send();
+      }else{
+        usersContainer.innerHTML = '';
+      }
     });
 </script>
--- other/project.sql
+++ other/project.sql
@@ -9,20 +9,20 @@ CREATE TABLE `account` (
   `data_nascita` date NOT NULL,
   `telefono` varchar(15) NOT NULL,
   `email` varchar(30) NOT NULL,
-  `password` varchar(20) NOT NULL
+  `password` varchar(260) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO `account` (`ID`, `cognome`, `nome`, `data_nascita`, `telefono`, `email`, `password`) VALUES
-(1, 'Oberti', 'Boris', '1977-10-26', '081/140336', 'boris.ardengo@teletu.it', 'BS27rozdD52D'),
-(3, 'Pirandelli', 'Barnaba', '1963-07-17', '011/597834', 'barnaba.pirandelli@aruba.it', 'ZG15kctkR56A'),
-(4, 'Asinari', 'Piera', '2001-06-15', '045/510012', 'piera.asinari@teletu.it', 'GW23jqttU72P'),
-(5, 'Zanco', 'Guglielmo', '1952-02-20', '0131/527077', 'guglielmo.zanco@katamail.it', 'IN37nqbzF54I'),
-(6, 'Ardovini', 'Valeria', '1994-09-05', '010/409715', 'valeria.ardovini@gmail.it', 'FT00mmxaI51F'),
-(7, 'Danova', 'Goffredo', '2020-04-27', '070/343016', '', 'NL30lefzP74I'),
-(8, 'Aiana', 'Ildebrando', '1959-11-29', '031/186551', 'ildebrando.aiana@hotmail.com', 'RD89bjhyX80D'),
-(9, 'Tarantino', 'Mattia', '1985-07-28', '0761/150121', 'mattia.tarantino@tele2.it', 'XY06bzplC95J'),
-(10, 'Rosellini', 'Rosa', '1962-12-19', '0422/940930', 'rosa.rosellini@virgilio.it', 'BN20doosW02B'),
-(11, 'Gherardi', 'Daniele', '2005-04-26', '3207256463', 'dani.ghera05@gmail.com', 'daniele');
+-- INSERT INTO `account` (`ID`, `cognome`, `nome`, `data_nascita`, `telefono`, `email`, `password`) VALUES
+-- (1, 'Oberti', 'Boris', '1977-10-26', '081/140336', 'boris.ardengo@teletu.it', 'BS27rozdD52D'),
+-- (3, 'Pirandelli', 'Barnaba', '1963-07-17', '011/597834', 'barnaba.pirandelli@aruba.it', 'ZG15kctkR56A'),
+-- (4, 'Asinari', 'Piera', '2001-06-15', '045/510012', 'piera.asinari@teletu.it', 'GW23jqttU72P'),
+-- (5, 'Zanco', 'Guglielmo', '1952-02-20', '0131/527077', 'guglielmo.zanco@katamail.it', 'IN37nqbzF54I'),
+-- (6, 'Ardovini', 'Valeria', '1994-09-05', '010/409715', 'valeria.ardovini@gmail.it', 'FT00mmxaI51F'),
+-- (7, 'Danova', 'Goffredo', '2020-04-27', '070/343016', '', 'NL30lefzP74I'),
+-- (8, 'Aiana', 'Ildebrando', '1959-11-29', '031/186551', 'ildebrando.aiana@hotmail.com', 'RD89bjhyX80D'),
+-- (9, 'Tarantino', 'Mattia', '1985-07-28', '0761/150121', 'mattia.tarantino@tele2.it', 'XY06bzplC95J'),
+-- (10, 'Rosellini', 'Rosa', '1962-12-19', '0422/940930', 'rosa.rosellini@virgilio.it', 'BN20doosW02B'),
+-- (11, 'Gherardi', 'Daniele', '2005-04-26', '3207256463', 'dani.ghera05@gmail.com', 'daniele');
 
 CREATE TABLE `movement` (
   `ID` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -57,13 +57,13 @@ INSERT INTO `valuta` (`COD`, `NOME`, `SIMBLO`) VALUES
 ('SEK', 'Corona svedese', 'kr'),
 ('USD', 'dollaro', '$');
 
-INSERT INTO `movement` (`ID`, `ACCOUNT_ID`, `VALUTA_COD`, `TIPO`, `AMMONTARE`, `DATA`) VALUES
-(1, 5, 'eur', 'salary', 5000, '2023-11-01'),
-(2, 5, 'eur', 'technology', -1000, '2023-11-02'),
-(3, 5, 'eur', 'extra', 100, '2023-11-03'),
-(4, 1, 'eur', 'food', -500, '2023-11-04'),
-(5, 5, 'CHF', 'extra', 200, '2023-11-06'),
-(6, 5, 'DKK', 'tip', 300, '2023-11-06');
+-- INSERT INTO `movement` (`ID`, `ACCOUNT_ID`, `VALUTA_COD`, `TIPO`, `AMMONTARE`, `DATA`) VALUES
+-- (1, 5, 'eur', 'salary', 5000, '2023-11-01'),
+-- (2, 5, 'eur', 'technology', -1000, '2023-11-02'),
+-- (3, 5, 'eur', 'extra', 100, '2023-11-03'),
+-- (4, 1, 'eur', 'food', -500, '2023-11-04'),
+-- (5, 5, 'CHF', 'extra', 200, '2023-11-06'),
+-- (6, 5, 'DKK', 'tip', 300, '2023-11-06');
 
 -- try CREATE TABLE IF NOT EXISTS `abuses`
 
