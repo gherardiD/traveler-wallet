001b877b2baed1d38500a3555b0ebaa1f68be143
gherardiD <gherardi.daniele.studente@itispaleocapa.it>
2024-01-10 13:44:58 +0100

    add: style landing page

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -7,7 +7,7 @@ import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="bg-neutral-400 w-full overflow-hidden flex h-screen">
+    <div className="bg-neutral-400 w-full flex h-screen">
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
