32ec70081acbd8e25364833295728752c85a12ea
gherardiD <gherardi.daniele.studente@itispaleocapa.it>
2024-04-03 13:56:23 +0200

    style: notFound page

--- V1/front-end/tailwind.config.js
+++ V1/front-end/tailwind.config.js
@@ -8,6 +8,12 @@ export default {
   ],
   theme: {
     extend: {},
+    screens: {
+      'sm': '641px',
+      'md': '769px',
+      'lg': '1025px',
+      'xl': '1281px',
+    },
   },
   plugins: [formsPlugin],
 }
