a2707b77dd781a443e8971f34695503125de1e1d
gherardiD <gherardi.daniele.studente@itispaleocapa.it>
2024-04-09 10:10:21 +0200

    v1

--- V2/back-end/routes/bankRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const { getBankBySlug } = require("../controllers/bankController");
-
-const router = express.Router();
-
-router.route("/:slug").get(getBankBySlug);
-
-module.exports = router;
--- V2/front-end/src/pages/ChooseBank.jsx
+++
@@ -1,9 +0,0 @@
-function ChooseBank() {
-  return (
-    <div className="w-full h-auto">
-      scegli la banca
-    </div>
-  );
-}
-
-export default ChooseBank;
