6b69be1f551a2f4031738f0d6b2b0595eb0757c6
gherardiD <101709520+gherardiD@users.noreply.github.com>
2024-04-30 08:58:00 +0000

    accesso server with workspace

--- front-end/src/api/Axios.jsx
+++ front-end/src/api/Axios.jsx
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const Axios = axios.create({
-  baseURL: `http://127.0.0.1:3001/api`,
+  // SERVER LOCAL
+  // baseURL: `http://127.0.0.1:3001/api`,
+  // SERVER CODESPACE
+  baseURL: "https://jubilant-zebra-gvpw9j7qp74hwjg-3001.app.github.dev/api"
 });
 
 
