d7de3bb229289005c73e82741c60d80d21689b73
gherardiD <gherardi.daniele.studente@itispaleocapa.it>
2024-05-16 17:30:17 +0200

    update: server url

--- front-end/src/api/Axios.jsx
+++ front-end/src/api/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Axios = axios.create({
-  baseURL: `http://127.0.0.1:3001/api`,
+  baseURL: `https://traveler-wallet-server.onrender.com/api`,
 });
 
 export default Axios;
